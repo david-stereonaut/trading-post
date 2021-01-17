ed915964e307f1a102330abe7468de89c48ac553
Ido Erez <idodoerez@gmail.com>
2021-01-17 22:35:41 +0200

    finished put routes + checking two of them

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,9 +1,27 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 const Schema = mongoose.Schema
-
 const userSchema = new Schema({
-
+    email: String,
+    password: String,
+    profilePic: String,
+    description: String,
+    images: [],
+    firstName: String,
+    lastName: String,
+    location: {
+        country: String,
+        city: String,
+        street: String
+    },
+    offeringTags: [],
+    seekingTags: [],
+    offering: [{ type: Schema.Types.ObjectId, ref: 'TradeCard' }], 
+    seeking : [{ type: Schema.Types.ObjectId, ref: 'TradeCard' }], 
+    conversations: [{ type: Schema.Types.ObjectId, ref: 'Conversation' }],
+    content: [],
+    reviews: [],
 }, { versionKey: false })
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema)
+
 module.exports = User
