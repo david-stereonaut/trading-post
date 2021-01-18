b456d25438bce26a2bf1b00a6645cef9a0e5f521
dayanidan007 <dayanidan007@gmail.com>
2021-01-18 19:43:56 +0200

    finish  all Repairs userapi

--- server/routes/userApi.js
+++ server/routes/userApi.js
@@ -5,122 +5,121 @@ const TradeCard = require('../models/TradeCardModel')
 const User = require('../models/UserModel')
 
 router.get('/myuser/:userId', async function (req, res) {
-    const {userId} = req.params
+    const { userId } = req.params
     const data = await User.findById(userId).populate('offering').populate('seeking')
     console.log(data)
-        let {_id, email,firstName,lastName,location,offeringTags,seekingTags,offering,seeking,conversations,content,reviews,profilePic, images, description} = data
-            const newData = {
-                _id,
-                email,
-                firstName,
-                lastName,
-                location,
-                offeringTags,
-                seekingTags,
-                offering,
-                seeking,
-                conversations,
-                content,
-                reviews,
-                profilePic,
-                images,
-                description
-            }
-            res.send(newData)
+    let { _id, email, firstName, lastName, location, offeringTags, seekingTags, offering, seeking, conversations, content, reviews, profilePic, images, description } = data
+    const newData = {
+        _id,
+        email,
+        firstName,
+        lastName,
+        location,
+        offeringTags,
+        seekingTags,
+        offering,
+        seeking,
+        conversations,
+        content,
+        reviews,
+        profilePic,
+        images,
+        description
+    }
+    res.send(newData)
 })
 
 router.get('/user/:userId', async function (req, res) {
-    const {userId} = req.params
+    const { userId } = req.params
     const data = await User.findById(userId).populate('offering').populate('seeking')
-        let {_id, email,firstName,lastName,location,offeringTags,seekingTags, offering, seeking, content,reviews,profilePic,images, description} = data
-            const newData ={
-                _id,
-                email,
-                firstName,
-                lastName,
-                location,
-                offeringTags,
-                seekingTags,
-                offering,
-                seeking,
-                content,
-                reviews,
-                profilePic,
-                images,
-                description
-            }
-            res.send(newData)
-    })
-    
-    router.post('/user', function (req, res) {
-       let data = req.body
-       const newData = new User({...data})
-        newData.save()
-            .then(response => {
-                console.log('add user')
-            })
-        res.send("1")
-    })
-       
-    // router.post('/tradecard', function (req, res) {
-    //    let data = req.body
-    //    const newData = new TradeCard({...data})
-    //     newData.save()
-    //         .then(response => {
-    //             console.log('add tradecard')
-    //         })
-    //     res.send("1")
-    // })
+    let { _id, email, firstName, lastName, location, offeringTags, seekingTags, offering, seeking, content, reviews, profilePic, images, description } = data
+    const newData = {
+        _id,
+        email,
+        firstName,
+        lastName,
+        location,
+        offeringTags,
+        seekingTags,
+        offering,
+        seeking,
+        content,
+        reviews,
+        profilePic,
+        images,
+        description
+    }
+    res.send(newData)
+})
 
-      
-    // router.post('/conversation', function (req, res) {
-    //     let data = req.body
-    //    const newData = new Conversation({...data})
-    //     newData.save()
-    //         .then(response => {
-    //             console.log('add conversation')
-    //         })
-    //     res.send("1")
-    // })
+router.post('/user', async function (req, res) {
+    let data = req.body
+    try {
+        const userUploaded = new User({ ...data })
+        await userUploaded.save()
+        res.send(userUploaded)
+    }
+    catch (err) {
+        res.send(err.message)
+    }
+})
 
-  
-   router.put('/addToUserArray/:userId', function (req, res) {
-    let {keyName, value} = req.body 
-    let {userId} = req.params
-        User.findOneAndUpdate({ _id: userId }, { $push: { [keyName]: value }}, function (error, success){
-            if (error) {
-                console.log(error);
-            } else {
-                console.log(success)
-            }
-        })
-        res.send("1")
-    })
+// router.post('/tradecard', function (req, res) {
+//    let data = req.body
+//    const newData = new TradeCard({...data})
+//     newData.save()
+//         .then(response => {
+//             console.log('add tradecard')
+//         })
+//     res.send("1")
+// })
 
-    router.put('/removeFromUserArray/:userId', function (req, res) {
-        let {keyName, value} = req.body 
-        let {userId} = req.params
-        User.findOneAndUpdate({ _id: userId }, { $pull: { [keyName]: value } }, function (error, success){
-            if (error) {
-                console.log(error);
-            } else {
-                console.log(success)
-            }
-        })
-        res.send("1")
-    })
 
-    router.put('/updateUserDetails/:userId', function (req, res) {
-        let {keyName, value} = req.body 
-        let {userId} = req.params
-        User.findOneAndUpdate({ _id: userId },  {$set: {[keyName]: value}}, function (error, success){
-            if (error) {
-                console.log(error);
-            } else {
-                console.log(success)
-            }
-        })
-        res.send("1")
-    })
-  
+// router.post('/conversation', function (req, res) {
+//     let data = req.body
+//    const newData = new Conversation({...data})
+//     newData.save()
+//         .then(response => {
+//             console.log('add conversation')
+//         })
+//     res.send("1")
+// })
+
+
+router.put('/addToUserArray/:userId',async function (req, res) {
+    let { keyName, value } = req.body
+    let { userId } = req.params
+    try {
+        const updatedUser = await User.findOneAndUpdate({ _id: userId }, { $push: { [keyName]: value } }, { new: true })
+        res.send(updatedUser)
+    }
+    catch (err) {
+        res.send(err.message)
+    }
+})
+
+router.put('/removeFromUserArray/:userId',async function (req, res) {
+    let { keyName, value } = req.body
+    let { userId } = req.params
+    try {
+        const updatedUser = await User.findOneAndUpdate({ _id: userId }, { $pull: { [keyName]: value } }, { new: true })
+        res.send(updatedUser)
+    }
+    catch (err) {
+        res.send(err.message)
+    }
+})
+
+router.put('/updateUserDetails/:userId', async function (req, res) {
+    const { keyName, value } = req.body
+    const { userId } = req.params
+    try {
+        const updatedUser = await User.findOneAndUpdate({ _id: userId }, { $set: { [keyName]: value } }, { new: true })
+        res.send(updatedUser)
+    }
+    catch (err) {
+        res.send(err.message)
+    }
+})
+
 module.exports = router
